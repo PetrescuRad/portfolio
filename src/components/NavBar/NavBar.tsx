fc6440bf6b849118f8013726bbfaa1cc2fee7821
PetrescuRad <radu.petrescu@mail.mcgill.ca>
2025-10-24 17:10:06 -0400

    removed import

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -4,7 +4,6 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
-import { Link } from 'react-router-dom';
 import './NavBar.css';
 
 const NavBar = () => {
